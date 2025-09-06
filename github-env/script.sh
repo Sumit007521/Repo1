2e00217945a5606dd02650b471b1a9a3218c06b0
Sonu <sumitsharmafb2000@gmail.com>
2025-09-07 00:56:58 +0530

    refactor: uncomment output of environment variables to GITHUB_ENV

--- github-env/script.sh
+++ github-env/script.sh
@@ -5,4 +5,4 @@ echo "Environment Name: $ENV_NAME"
 
 ENV_VARS=$(gh variable list --json name,value -R $REPO -e $ENV_NAME) 
 echo "$ENV_VARS" >> env_var.json
-# echo "$ENV_VARS" | jq -r '.[]|"\(.name)=\(.value)"' >> $GITHUB_ENV  
+echo "$ENV_VARS" | jq -r '.[]|"\(.name)=\(.value)"' >> $GITHUB_ENV  
