a7ebfe33995de06189fe446f798109e17ea3b4a7
Sonu <sumitsharmafb2000@gmail.com>
2025-09-08 00:25:11 +0530

    refactor: add variable count check and output messages in script.sh

--- github-env/script.sh
+++ github-env/script.sh
@@ -5,4 +5,12 @@ echo "Environment Name: $ENV_NAME"
 
 ENV_VARS=$(gh variable list --json name,value -R $REPO -e $ENV_NAME) 
 echo "$ENV_VARS" >> env_var.json
-echo "$ENV_VARS" | jq -r '.[]|"\(.name)=\(.value)"' >> $GITHUB_ENV  
+
+var_count=$(cat env_var.json | jq 'length')
+
+if [ $var_count -gt 0 ]; then
+    echo "Number of variables retrieved: $var_count"
+    echo "$ENV_VARS" | jq -r '.[]|"\(.name)=\(.value)"' >> $GITHUB_ENV  
+else
+    echo "No variables retrieved: $var_count"
+fi
